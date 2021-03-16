25644e1874dec7e6bd946c3903581b4303504c51
杨凯 <yangkai01@chtwm.com>
2021-03-16 17:40:09 +0800

    Initial commit

---
+++ src/test/java/com/chtwm/xxl/job/demo/XxlJobDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chtwm.xxl.job.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XxlJobDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
