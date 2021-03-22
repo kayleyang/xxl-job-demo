0b0c627f18a652f0237be5a73d482bb0c1fc305c
杨凯 <yangkai01@chtwm.com>
2021-03-22 10:51:31 +0800

    run as none web

--- src/main/java/com/chtwm/xxl/job/demo/XxlJobDemoApplication.java
+++ src/main/java/com/chtwm/xxl/job/demo/XxlJobDemoApplication.java
@@ -1,13 +1,17 @@
 package com.chtwm.xxl.job.demo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class XxlJobDemoApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(XxlJobDemoApplication.class, args);
+    public static void main(String[] args) throws InterruptedException {
+        new SpringApplicationBuilder(XxlJobDemoApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        Thread.currentThread().join();
     }
 
 }
